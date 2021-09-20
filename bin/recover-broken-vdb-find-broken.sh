da320efe61609864f6987276b493563fe4c6b005
Hank Leininger <hlein@korelogic.com>
2021-09-20 17:16:55 -0600

    Accumulate an array and print if non-empty.
    The previous version would print a '=' by itself on a system with
    no affected packages. This also removes the subshell for readability.
    
    Signed-off-by: Hank Leininger <hlein@korelogic.com>

--- bin/recover-broken-vdb-find-broken.sh
+++ bin/recover-broken-vdb-find-broken.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 # Identify packages that provide .so files but are missing a PROVIDES file
 
-pkgs=$(cd /var/db/pkg && for A in */*/CONTENTS ; do
+declare -a pkgs
+
+cd /var/db/pkg && for A in */*/CONTENTS ; do
     # Iterate over all .sos they install
     for O in $(sed -n -E 's%^obj (/[^ ]+\.so( |\.[^ ]+)).*%\1%p' $A | sed 's/ $//') ; do
         SHARED=$(file "$O" | egrep 'shared object')
@@ -11,14 +13,15 @@ pkgs=$(cd /var/db/pkg && for A in */*/CONTENTS ; do
 
             # If it is, complain if we are missing the expected ELF metadata in the VDB
             if [ ! -f "${T}/PROVIDES" -o ! -f "${T}/NEEDED" -o ! -f "${T}/NEEDED.ELF.2" ]; then
-                echo "${T}"
+                # Remember this package with full version suitable for re-emerging
+                pkgs+=("=${T}")
             fi
 
             # We checked this package already so we can bail out
             break
         fi
     done
-done)
+done
 
-# Give exact versions suitable for emerge in output
-echo "${pkgs}" | sed -e 's:^:=:g'
+# Output the affected packages, if any
+if (( ${#pkgs[@]} )) ; then printf "%s\n" "${pkgs[@]}" ; fi
